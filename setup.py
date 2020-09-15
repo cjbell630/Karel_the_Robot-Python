7a79fe15fc8e0b55d105e7f292b7ab1e40a2621e
Connor Bell <cjbell630@users.noreply.github.com>
2020-09-14 20:30:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class UploadCommand(Command):
 # This call to setup() does all the work
 setup(
     name="karel_the_robot",  # what will be typed when you use pip install
-    version="1.0.1",
+    version="1.1.0",
     description="Unofficial port of Karel the Robot to Python",
     long_description=README,
     long_description_content_type="text/markdown",
